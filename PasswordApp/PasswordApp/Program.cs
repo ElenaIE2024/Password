05280b8d0e2c1d8f9b2d3b0a2883e20d7094f8ae
Elena <elena711912@yandex.ru>
2026-03-29 20:25:08 +0300

    Добавлены два проекта: библиотека и приложение

---
+++ PasswordApp/PasswordApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordLibrary;
+
+
+namespace PasswordApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите пароль: ");
+            string s = Console.ReadLine();
+            Password password = new Password(s);
+            if (password.CheckPassword())
+                Console.WriteLine("Yes!");
+            else
+                Console.WriteLine("No...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PasswordLibrary/PasswordLibrary/Password.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordLibrary
+{
+    public class Password
+    {
+        string myPassword;
+        public Password(string password)
+        {
+            myPassword = password;
+        }
+        public bool CheckPassword()
+        {
+            Regex digit = new Regex(@"\d");
+            if (myPassword.Length < 6)
+                return false;
+            else
+                return digit.IsMatch(myPassword);
+        }
+    }
+}
