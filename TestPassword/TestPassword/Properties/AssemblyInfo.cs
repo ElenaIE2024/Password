816914f1e31eee792429a2a46c6f0dd6cde058c4
Elena <elena711912@yandex.ru>
2026-04-02 22:05:02 +0300

    Добавлено модульное тестирование

---
+++ TestPassword/TestPassword/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestPassword")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPassword")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5201b1b-a609-4ec1-ba6f-0cb45117ee3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestPassword/TestPassword/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PasswordLibrary;
+
+
+namespace TestPassword
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PasswordNOKLength()
+        {
+            Password psw = new Password("7ff");
+            Assert.IsFalse(psw.CheckPassword());
+        }
+        [TestMethod]
+        public void PasswordOKNumberNotFirst()
+        {
+            Password psw = new Password("ffii5iiiiii");
+            Assert.IsTrue(psw.CheckPassword());
+        }
+        [TestMethod]
+        public void PasswordOKNumberFirst()
+        {
+            Password psw = new Password("7ffiiiiiiii");
+            Assert.IsTrue(psw.CheckPassword());
+        }
+    }
+}
